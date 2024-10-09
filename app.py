76d67b701217f37785363d205c7d12ba31cc2097
Faiq Imran <87329733+FaiqImran123@users.noreply.github.com>
2024-10-10 01:17:28 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import re
+import pickle
+import streamlit as st
+
+
+st.title("Spam Classifier😊")
+
+
+sentence =st.text_input("Enter Message")
+
+# cleaning and preprocessing
+
+sentence =re.sub("[^a-zA-Z]", " ", sentence)
+sentence =sentence.lower().split()
+sentence =[WordNetLemmatizer().lemmatize(word) for word in sentence  if word not in stopwords.words("english")]
+sentence =" ".join(sentence)
+
+
+# importing tf-idf, model
+with open("model.pkl", "rb") as f:
+    model =pickle.load(f)
+
+with open("tf_idf_object.pkl", "rb") as f:
+    tf_idf =pickle.load(f)
+
+
+# transoforming input
+sentence =tf_idf.transform([sentence]).toarray()
+
+# prediction
+if model.predict(sentence)==1:
+    st.write("Spam")
+else:
+    st.write("Ham")
+
+
+
+    
+
